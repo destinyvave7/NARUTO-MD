454a61ae0d19a28692be784914421ef941aaa848
destinyvave7 <jackdestiny376@gmail.com>
2024-12-21 06:54:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "EC1l1RKZ#iIk20jIp2r7-3tmLs0h2qfqTCnPfenAgsis_CVXc-3w",
+SESSION_ID: process.env.SESSION_ID || "LEZDAISa#64lyPs3CRNKs4yFn9g-_md1jF87r4mBeTcYAkVO0HW4",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/HO8U7W1.jpeg",
 SUDO_NB: process.env.SUDO_NB || "2349073081822",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
